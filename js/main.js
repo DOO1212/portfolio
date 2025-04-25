da5d0e7416fb716010e492c23bada71b4ff3ae61
portfolio <ejejgus@naver.com>
2025-04-25 17:12:46 +0900

    tt

--- js/main.js
+++ js/main.js
@@ -50,7 +50,7 @@ scroll.forEach(function (spy, index) {
   // 메소드 체이닝
   new ScrollMagic.Scene({ // 감시할 장면 추가 및 옵션 지정
     triggerElement: spy, // 보여짐 여부를 감시할 요소를 지정
-    triggerHook: 0.6 // 화면의 50% 지점에서 보여짐 여부 감시(0~1사이 지정)
+    triggerHook: 0.7 // 화면의 50% 지점에서 보여짐 여부 감시(0~1사이 지정)
   })
   .setClassToggle(spy, 'show') // 요소가 화면에 보이면 show 클래스 추가
   .addTo(controller); // 컨트롤러에 장면을 할당(필수) - 라이브러리에서 지정한 문법으로 깊게 이해x
